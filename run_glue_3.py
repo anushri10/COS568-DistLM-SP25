2dff0e2f1cdd1d2c332191a70785b115ea2d569b
Anushri Arora <anushriarora@Anushris-MacBook-Air.local>
2025-03-28 16:27:16 -0400

    q3 v2

--- run_glue_3.py
+++ run_glue_3.py
@@ -354,7 +354,7 @@ def main():
     # Setup logging
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                         datefmt='%m/%d/%Y %H:%M:%S',
-                        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+                        level=logging.INFO)
     logger.warning("Process rank: %s, device: %s, distributed training: %s, fp16: %s",
                    args.local_rank, args.device, (args.world_size > 1), args.fp16)
 
